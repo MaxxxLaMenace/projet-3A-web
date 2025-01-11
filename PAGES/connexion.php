2e4b0d952d556f6c6995b0b138b412e32f65901b
EricLefebvre <eric@lefebvre-famille.fr>
2025-01-12 00:05:25 +0100

    Ajout des composants utiles pour toutes les pages navbar footer dans la page connexion comme exemple

--- PAGES/connexion.php
+++ PAGES/connexion.php
@@ -39,46 +39,39 @@
     }
 ?>
 
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <title>Inscription</title>
-    <link href="../CSS/style.css" rel="stylesheet" media="all">
-    <link href="../CSS/inscription.css" rel="stylesheet" media="all">
-</head>
-<body>
-    <header>
-        <div class="logo">
-            <a href="draft.html"><img src="../Annexes/logo_site.png" alt="Logo"></a>
-        </div>
-        <div class="title">
-            <h1>Human Benchmark</h1>
-        </div>
-        <div class="buttons">
-            <a href="inscription.html"><button>S'inscrire</button></a>
-            <a href="connexion.html"><button>Se connecter</button></a>
-        </div>
-    </header>
+<?php require_once("../Components/header.html"); ?>
 
-    <main class="page-inscription">
-        <h2 class="inscription">Connexion</h2>
-        <form action="connexion.php" method="POST">
-            
-            <?php if (isset($errorMsg) && !empty($errorMsg)): ?>
-                <p style="color: red;"><?php echo $errorMsg; ?></p>
-            <?php endif; ?>
-            
-            <label for="username">Nom d'utilisateur * :</label><br><br>
-            <input type="text" id="username" name="username" required>
-            
-            <label for="password">Mot de passe * :</label><br><br>
-            <input type="password" id="password" name="password" required>
-            
-            <button type="submit">Se connecter</button>
-        </form>
-    </main>
-    <p class="mention">Champs marqués (*) obligatoires.<br>Les autres informations ne seront pas sauvegardés.</p>
+<head>
+    <link href="../CSS/inscription.css" rel="stylesheet" media="all">
     <script src="../JS/inscription.js"></script>
+</head>
+
+<?php require_once("../Components/navbar.html"); ?>
+
+<body>
+    <div class="content">
+        <main class="page-inscription">
+            <h2 class="inscription">Connexion</h2>
+            <form action="connexion.php" method="POST">
+                
+                <?php if (isset($errorMsg) && !empty($errorMsg)): ?>
+                    <p style="color: red;"><?php echo $errorMsg; ?></p>
+                <?php endif; ?>
+                
+                <label for="username">Nom d'utilisateur * :</label><br><br>
+                <input type="text" id="username" name="username" required>
+                
+                <label for="password">Mot de passe * :</label><br><br>
+                <input type="password" id="password" name="password" required>
+                
+                <button type="submit">Se connecter</button>
+            </form>
+        </main>
+
+        <p>Champs marqués (*) obligatoires.<br>Les autres informations ne seront pas sauvegardés.</p>
+
+    </div>
+
+    <?php require_once("../Components/footer.html"); ?>
 </body>
-</html>
+
